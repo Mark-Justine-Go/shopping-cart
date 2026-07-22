fedb49086ddda71232819ea2ca797168311dfaf0
Mark-Justine-Go <118537589+Mark-Justine-Go@users.noreply.github.com>
2026-07-22 13:54:56 +0800

    feat: improve homepage ui

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from 'react-router';
 import "./styles/home.css"
 import hero1 from "./assets/hero1.jpg"
 import hero2 from "./assets/hero2.jpg"
@@ -7,6 +8,24 @@ import hero3 from "./assets/hero3.jpg"
 function Home(){
 
     const [currHero, changeHero] = useState(hero1);
+    const [buttonClasses, setButtonClasses] = useState(["selected","",""]);
+
+    function changeHeroState(idx){
+        switch(idx){
+            case 1:
+                changeHero(hero1);
+                setButtonClasses(prev => ["selected","",""]);
+                break
+            case 2:
+                changeHero(hero2);
+                setButtonClasses(prev => ["","selected",""]);
+                break
+            case 3:
+                changeHero(hero3);
+                setButtonClasses(prev => ["","","selected"]);
+                break
+        }
+    }
 
     return(
         <div id="home">
@@ -14,17 +33,17 @@ function Home(){
                 <p>Welcome to Virtual Vault! Here at Virtual Vault, you can find 
                     different merchandise related to your favorite vtubers. We 
                     offer a wide variety of items ranging from a simple keychain 
-                    to a full blown sets. We are dedicated to providing an easy and 
+                    to a full blown sets. We are dedicated at providing an easy and 
                     affordable service no matter where you are. </p>
-                    <button>Go To Shop</button>
+                    <button><Link to="shop">Go To Shop</Link></button>
             </div>
             <div id="right">
                 <img src={currHero} alt="" />
                 <div>
                     <ul>
-                        <li><button onClick={(e)=>changeHero(hero1)}></button></li>
-                        <li><button onClick={(e)=>changeHero(hero2)}></button></li>
-                        <li><button onClick={(e)=>changeHero(hero3)}></button></li>
+                        <li><button className={buttonClasses[0]} onClick={(e)=>changeHeroState(1)}></button></li>
+                        <li><button className={buttonClasses[1]} onClick={(e)=>changeHeroState(2)}></button></li>
+                        <li><button className={buttonClasses[2]} onClick={(e)=>changeHeroState(3)}></button></li>
                     </ul>
                 </div>
             </div>
